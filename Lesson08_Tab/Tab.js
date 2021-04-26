b081687bdad36ea4dd457d7217f4def15db93549
qixiang <qixiang1009@gmail.com>
2021-04-26 23:55:03 +0800

    Updated. 2021/04/26

---
+++ Lesson08_Tab/Tab.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#contents div[id != "tab1"]').hide()
+    $('a').click(function(){
+
+        $('#contents div').hide()
+        
+        $($(this).attr('href')).show()
+
+        $('.current').removeClass('current')
+
+        $(this).parent().addClass('current')
+
+        return false;
+    })
+})
