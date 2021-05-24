c6f64ba597ea7b3e3680c130d61f44f80cc1538c
XIANG <qixiang1009@gmail.com>
2021-05-24 17:12:18 +0800

    Updated. 2021/05/24

---
+++ Lesson12_Photo_Caption/PhotoCaption.js
@@ -0,0 +1,17 @@
+$(window).on("load",function(){
+    $(".loader-wrap").fadeOut();
+})
+$(document).ready(function(){
+    $('li').hover(function(){
+        $(this).append("<div><p>"+$(this).children('img').attr('alt')+"</p></div>")
+        $(this).children('div').stop().fadeIn(300);
+        $(this).children('div').children('p').stop().animate({"top":"0px"},300)
+
+    },function(){
+        $(this).children('div').stop().fadeOut(300);
+        $(this).children('div').children('p').stop().animate({"top":"10px"},300,function(){
+            $(this).parent('div').remove();
+        })
+
+    });
+});
